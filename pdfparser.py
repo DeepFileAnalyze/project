acf20c7342ba2042c997ce47c5a0dcdfc7c01129
Panda <panda@alenan.org>
2018-03-25 05:58:33 +0200

    Write pdfparser.py OCR and Search functionality

--- pdfparser.py
+++ pdfparser.py
@@ -16,6 +16,7 @@ from PIL import Image as PI
 
 indexdir = "/home/kaese/git/project/out/index"
 textsdir = "/home/kaese/git/project/out/texts"
+pdfsdir = "/home/kaese/git/project/out/pdfs"
 searchdirs = []
 
 class Helper:
@@ -23,11 +24,31 @@ class Helper:
         if not os.path.exists(path):
             os.makedirs(path)
     def add_to_searchdirs(self, path):
-        searchdirs.append(path)
+        if path not in searchdirs:
+            searchdirs.append(path)
+    def write_to(self, string, path):
+        f = open(path, 'w+')
+        f.write(string)
+        f.close()
+    def build_searchdirs_from_existing(self):
+        for directory in os.listdir(textsdir):
+            path = textsdir + "/" + directory
+            if os.path.isdir(path):
+                self.add_to_searchdirs(path)
 
 
 class OCR:
-    def pdf_to_jpegs(self):
+    def pdfs_to_texts(self, path):
+        req_images = []
+        h = Helper()
+        h.mkdir_if_not_exist(path)
+        pdfnames = os.listdir(path)
+        for filename in pdfnames:
+            req_images.append(self.pdf_to_jpegs(path + "/" + filename))
+        for i, images in enumerate(req_images):
+            self.jpegs_to_texts(images, pdfnames[i])
+
+    def pdf_to_jpegs(self, path):
         req_image = []
         image_pdf = Image(filename=path, resolution=300)
         image_jpeg = image_pdf.convert('jpeg')
@@ -36,12 +57,16 @@ class OCR:
             req_image.append(img_page.make_blob('jpeg'))
         return req_image
 
-    def jpegs_to_texts(self, req_image):
+    def jpegs_to_texts(self, req_image, pdfname):
         h = Helper()
-        h.mkdir_if_not_exist(textsdir)
-        for img in req_image: 
+        h.mkdir_if_not_exist(textsdir + "/" + pdfname)
+        tool = pyocr.get_available_tools()[0]
+        lang = tool.get_available_languages()[0]
+        for i, img in enumerate(req_image): 
             txt = tool.image_to_string(PI.open(io.BytesIO(img)), lang=lang, builder=pyocr.builders.TextBuilder())
-
+            path = textsdir + "/" + pdfname  + "/" + str(i)
+            h.write_to(txt, path)
+            h.add_to_searchdirs(textsdir + "/" + pdfname)
 
 class SampleSchema(SchemaClass):
     title = TEXT(stored=True)
@@ -79,7 +104,12 @@ class Search:
     
 
 if __name__ == '__main__':
+#    o = OCR()
+#    o.pdfs_to_texts(pdfsdir)
+    h = Helper()
+    h.build_searchdirs_from_existing()
+    print(searchdirs)
     s = Search()
     s.create_index()
 
-    print(s.search("penis"))
+    print(s.search("Routing"))
